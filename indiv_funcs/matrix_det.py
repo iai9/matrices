1a9bb983297a242b26b9864fac7f102c978eaa5b
Imran Iftikar <imran.aidan.ai@gmail.com>
2021-11-10 10:09:46 -0500

    created determinant file

---
+++ indiv_funcs/matrix_det.py
@@ -0,0 +1,19 @@
+'''
+11.10.21
+imran iftikar
+
+the purpose of this program is to find the determinant of TWO DIMENSIONAL matrix in upper-triangle form
+
+    array = [
+            [a1, a2, a3, a4, ..., aN],
+            [0, b2, b3, b4, ..., bN],
+            [0, 0, c3, c4, ..., cN],
+            ...,
+            [0, 0, 0, 0, ..., zN]
+    ]
+
+i've already made a program to find the echelon form of a matrix, so I don't need to worry about that. 
+Here, I will create
+
+
+'''
