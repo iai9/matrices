cc3766700316225174339ca3750802d407b33555
Imran Iftikar <imran.aidan.ai@gmail.com>
2021-11-10 11:55:41 -0500

    done with det

--- indiv_funcs/matrix_det.py
+++ indiv_funcs/matrix_det.py
@@ -2,7 +2,7 @@
 11.10.21
 imran iftikar
 
-the purpose of this program is to find the determinant of TWO DIMENSIONAL matrix in upper-triangle form
+the purpose of this program is to find the determinant of TWO DIMENSIONAL matrix IN UPPER TRIANGLE ECHELON FORM
 
     array = [
             [a1, a2, a3, a4, ..., aN],
@@ -13,7 +13,34 @@ the purpose of this program is to find the determinant of TWO DIMENSIONAL matrix
     ]
 
 i've already made a program to find the echelon form of a matrix, so I don't need to worry about that. 
-Here, I will create
+Here, I will find det by iterating through a list in echelon form
 
 
 '''
+
+######### Dependencies
+
+'''none'''
+
+######### Funcs
+
+def matrix_det(matrix):
+
+    if len(matrix) == len(matrix[0]):
+
+        det = 1
+        for diag_index in range(len(matrix)):
+            det = det*matrix[diag_index][diag_index]
+    
+        return det
+    
+    else:
+        print(f"Error: argument expected to be square")
+
+######### Vars
+
+m1 = [[27, 5, 6], [0.0, 5.2592592592592595, 1.1111111111111112], [0.0, 0.0, 4.661971830985916]]
+
+######### Main
+
+print(matrix_det(m1))
