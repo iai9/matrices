e89bf7e2c75191dcd00ad12abb145266943fb993
Imran Iftikar <imran.aidan.ai@gmail.com>
2021-11-17 12:22:54 -0500

    _

--- indiv_funcs/inverse_matrix.py
+++ indiv_funcs/inverse_matrix.py
@@ -5,6 +5,27 @@ no. decided not to do
 
 '''
 
-x = 0 
+pi = 3.1415926535
 
-print(isinstance(x, int))
+def factorial(number):
+
+    if isinstance(number, int) and number > 1:
+        initial_product = 1
+        for i in range(1, number+1):
+            initial_product = initial_product*i
+        
+        return initial_product
+    else:
+        raise ValueError("Number cannot be factorial-ed")
+
+def to_rad(number): # hard coding, not iterating. idrc that the taylor series is infinite, I'll get close enough
+    return number/pi
+
+def rad_simplify(rad):
+    pass
+
+def sin(x):
+    h =  x - ((x**3)/(factorial(3))) + ((x**5)/(factorial(5))) - ((x**7)/(factorial(7))) + ((x**9)/(factorial(9))) - ((x**11)/(factorial(11)))
+    return h
+
+print(sin(50))
